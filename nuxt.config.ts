24ffa43c2206b9d8f4e46f17f4b72964986c9ddd
VLAD F <woohda@ya.ru>
2026-09-17 16:50:14 +0700

    feat: добавил плавную анимацию переключения страниц

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,8 @@ export default defineNuxtConfig({
     },
   },
   app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    layoutTransition: { name: "page", mode: "out-in" },
     head: {
       meta: [
         {
