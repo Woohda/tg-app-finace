17214f51abea38a40ee265c6a5ac29786f3708af
VLAD F <woohda@ya.ru>
2026-09-21 15:13:15 +0700

    Исправление: возврат -webkit-mask-image (убрано ошибочное -webkit-compatibility)

--- app/components/analytics/LiquidSphere.vue
+++ app/components/analytics/LiquidSphere.vue
@@ -72,8 +72,10 @@ const liquidClassBase = computed(() =>
 <style scoped>
 .safari-clip-fix {
   /* Safari fix for overflow: hidden with border-radius and CSS transforms */
-  -webkit-compatibility: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=");
-  -webkit-compatibility: hidden;
+  -webkit-mask-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=");
+  mask-image: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=");
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
   transform: translateZ(0);
 }
 @keyframes wave-spin {
